7c95a9be45f6f88d76183e8f5c0e10f989731bb3
pample <bonnier.vincentpro@gmail.com>
2022-06-30 09:28:27 +0200

    clean space for new MVC

--- template/createFormView.php
+++
@@ -1,7 +0,0 @@
-<form action="processForm.php" method="POST">
-    <label>Prénom : </label>
-    <input type="text" placeholder="veuillez saisir votre prenom" name="key_prenom" />
-    <input type="text" placeholder="veuillez saisir votre prenom" name="key_nom" />
-    <input type="text" placeholder="veuillez saisir votre prenom" name="key_birthday" />
-    <input type="submit" value="publier"/>
-</form>
